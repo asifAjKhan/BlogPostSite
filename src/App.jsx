1710ac082daaae497b225cbbca0599a37693af21
asifAjKhan <akhan006584@gmail.com>
2024-11-29 15:44:26 +0600

    navbar and hambarger menu design

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Navbar from "./components/Navbar"
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='px-4 md:px-8 lx:px-32 2xl:px-64'>
+      {/* NAVBAR */}
+      <Navbar />
+      {/* BREADCRUMB */}
+      {/* FEATURED POSTS */}
+      {/* POST LIST */}
+    </div>
   )
 }
 
